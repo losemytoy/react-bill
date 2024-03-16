5dca65ad53262fe9cf59be11848b831b58d8f32b
linyiming <2391660155@qq.com>
2024-03-16 21:30:50 +0800

    基于当前月的统计数据计算

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -19,9 +19,23 @@ const Month = () => {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs(new Date()).format('YYYY-MM')
   })
+  const [currentMonthList, setMonthList] = useState([]);
+
+  const monthResult= useMemo(() => {
+    const pay = currentMonthList.filter(item => item.type === 'pay').reduce((a, c) => a + c.money, 0)
+    const income = currentMonthList.filter(item => item.type === 'income').reduce((a, c) => a + c.money, 0)
+    return {
+      pay,
+      income,
+      total: pay + income
+    }
+  }, [currentMonthList]);
+
   const onConfirm = (date) => {
-    setCurrentDate(dayjs(date).format('YYYY-MM'))
+    const formatDate = dayjs(date).format('YYYY-MM')
+    setCurrentDate(formatDate)
     setDateVisible(false)
+    setMonthList(monthGroup[formatDate]??[])
   }
   return (
     <div className="monthlyBill">
@@ -40,15 +54,15 @@ const Month = () => {
           {/* 统计区域 */}
           <div className='twoLineOverview'>
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{monthResult.pay}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.income}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{monthResult.total}</span>
               <span className="type">结余</span>
             </div>
           </div>

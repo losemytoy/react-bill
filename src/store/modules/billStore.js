3be2c9ea02917013bd91ca7c72b0f14ae4cc9c2b
linyiming <2391660155@qq.com>
2024-03-16 20:33:19 +0800

    月度菜单-切换时间显示

---
+++ src/store/modules/billStore.js
@@ -0,0 +1,31 @@
+//帐单列表相关store
+
+import {createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+
+const billStore = createSlice({
+  name: 'bill',
+  initialState: {
+    billList: []
+  },
+  reducers: {
+    setBillList(state, action) {
+      state.billList = action.payload
+    }
+  }
+})
+
+const {setBillList} = billStore.actions
+
+const getBillList = () => {
+  return async (dispatch) => {
+    const res = await axios.get('http://localhost:8888/ka')
+    dispatch(setBillList(res.data))
+  }
+}
+
+export {getBillList}
+
+const billReducer = billStore.reducer
+
+export default billReducer

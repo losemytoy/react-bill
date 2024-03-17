aec80944b71b8ac8128ed441890f11b90917e513
linyiming <2391660155@qq.com>
2024-03-17 15:42:43 +0800

    新增账单完善

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -9,6 +9,8 @@ import {billListData} from "../../contents";
 import {useState} from "react";
 import {useDispatch} from "react-redux";
 import {addBillList} from "../../store/modules/billStore";
+import {value} from "lodash/seq";
+import dayjs from "dayjs";
 
 const New = () => {
   const navigate = useNavigate()
@@ -26,12 +28,17 @@ const New = () => {
     const data = {
       type: billType,
       money: billType === 'pay' ? -money : +money,
-      date: new Date(),
+      date: date ?? new Date(),
       useFor: useFor
     }
     dispatch(addBillList(data))
   }
-
+  const [dateVisible, setDateVisible] = useState(false)
+  const [date, setDate] = useState()
+  const dateConfirm = (value) => {
+    setDate(value)
+    setDateVisible(false)
+  }
   return (
     <div className="keepAccounts">
       <NavBar className="nav" onBack={() => navigate(-1)}>
@@ -60,11 +67,13 @@ const New = () => {
           <div className="kaForm">
             <div className="date">
               <Icon type="calendar" className="icon"/>
-              <span className="text">{'今天'}</span>
+              <span className="text" onClick={() => setDateVisible(true)}>{dayjs(date).format('YYYY-MM-DD')}</span>
               <DatePicker
                 className="kaDate"
                 title="记账日期"
                 max={new Date()}
+                visible={dateVisible}
+                onConfirm={dateConfirm}
               />
             </div>
             <div className="kaInput">
@@ -92,7 +101,7 @@ const New = () => {
                     <div
                       className={classNames(
                         'item',
-                        ''
+                        useFor === item.type ? 'selected' : ''
                       )}
                       key={item.type}
                       onClick={() => setUseFor(item.type)}

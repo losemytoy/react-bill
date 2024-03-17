19fe803d081a10f992a276358ff6836abe9b31de
linyiming <2391660155@qq.com>
2024-03-17 14:17:35 +0800

    控制账单的显隐

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import './index.scss'
-import {useMemo} from "react";
+import {useMemo, useState} from "react";
+import {billTypeToName} from "../../../../contents";
 
 const DailyBill = ({date, billList}) => {
   const dayList = useMemo(() => {
@@ -11,15 +12,16 @@ const DailyBill = ({date, billList}) => {
       income,
       total: pay + income
     }
-  })
+  }, [billList])
 
+  const [visible, setVisible] = useState(false)
 
   return (
     <div className={classNames('dailyBill')}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames('arrow')}></span>
+          <span className={classNames('arrow',visible && 'expand')} onClick={() => setVisible(!visible)}></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -35,6 +37,21 @@ const DailyBill = ({date, billList}) => {
             <span className="type">结余</span>
           </div>
         </div>
+      </div>
+      {/* 单日列表 */}
+      <div className="billList" style={{display: visible ? 'block' : 'none'}}>
+        {billList.map(item => {
+          return (
+            <div className="bill" key={item.id}>
+              <div className="detail">
+                <div className="billType">{billTypeToName[item.useFor]}</div>
+              </div>
+              <div className={classNames('money', item.type)}>
+                {item.money.toFixed(2)}
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
